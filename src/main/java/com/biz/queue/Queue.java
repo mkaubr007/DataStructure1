d3923fecc58549fd6cc7282ac9d05fdf99914420
mkaubr007 <mkaubr007@gmail.com>
2021-09-10 13:55:38 +0530

    QueueAdd added

---
+++ src/main/java/com/biz/queue/Queue.java
@@ -0,0 +1,19 @@
+package com.biz.queue;
+
+import com.biz.linkedlist.LinkedList;
+import com.biz.linkedlist.Node;
+
+public class Queue {
+    private Node top;
+    private LinkedList linkedList;
+
+    public Queue(){
+        this.linkedList=new LinkedList();
+    }
+    public void push() {
+        linkedList.push(70);
+        linkedList.push(30);
+        top = linkedList.push(56);
+        linkedList.print();
+    }
+}
---
+++ src/main/java/com/biz/queue/QueueMain.java
@@ -0,0 +1,18 @@
+package com.biz.queue;
+
+import java.util.Scanner;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1 to add element");
+
+
+        Queue queue=new Queue();
+        switch (sc.nextInt()){
+            case 1:
+                queue.push();
+                break;
+        }
+    }
+}
